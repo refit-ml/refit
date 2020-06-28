2ce176a68f15135219bfec9c5c399a43899f5035
Matthew Alvarez <matt@mattjalvarez.com>
2020-06-28 09:39:57 -0500

    Refactoring infrastructure

--- inference/src/main/scala/org/example/Main.scala
+++ inference/src/main/scala/org/example/Main.scala
@@ -3,7 +3,8 @@ package org.example
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.streaming.connectors.pulsar.PulsarSourceBuilder
+import org.apache.flink.streaming.connectors.pulsar.{FlinkPulsarProducer, PulsarSourceBuilder}
+import org.apache.pulsar.client.impl.auth.AuthenticationDisabled
 
 
 //sensor_id,timestamp,temperature,pressure,wind
@@ -11,20 +12,38 @@ case class SensorData(sensor_id: Int, timestamp: String, temperature: Int, press
 
 object Main {
   def main(args: Array[String]) {
+    Thread.sleep(60000)
     val env = StreamExecutionEnvironment.getExecutionEnvironment()
+    val serviceUrl = "pulsar://localhost:6650"
+    val inputTopic = "persistent://sample/standalone/ns1/u"
+    val outputTopic = "persistent://sample/standalone/ns1/o"
+    val subscribtionName = "scala-sub-1"
 
     val src = PulsarSourceBuilder.builder(new SimpleStringSchema())
-      .serviceUrl("pulsar://pulsar:6650")
-      .topic("persistent://sample/standalone/ns1/u")
-      .subscriptionName("scala-sub-1")
+      .serviceUrl(serviceUrl)
+      .topic(inputTopic)
+      .subscriptionName(subscribtionName)
       .build()
 
     val input = env.addSource(src)
-
     input.print().setParallelism(1)
+
+
+
+    input.addSink(new FlinkPulsarProducer[String](
+      serviceUrl,
+      outputTopic,
+      new AuthenticationDisabled(),
+      (x) => x.hashCode()
+    ))
+
+
 
     env.execute("Test Job")
 
   }
 }
 
+class Transporter {
+
+}
