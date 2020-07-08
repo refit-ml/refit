5a85ca439ad7f346eecc59974bbd2695c9abc1e3
Matthew Alvarez <matt@mattjalvarez.com>
2020-07-08 14:23:59 -0500

    adding lazy list

--- data/src/main/scala/Main.scala
+++ data/src/main/scala/Main.scala
@@ -24,9 +24,11 @@ object Main {
       .map(x => new SensorData(x(0).toInt, x(1), x(2).toDouble, x(3).toDouble, x(4).toDouble))
 
     val client = PulsarClient("pulsar://localhost:6650")
-    lazy val iter2 = LazyList.continually( () => {
 
-    })
+    def lst: LazyList[SensorData] = {
+      def loop(v: Int): LazyList[SensorData] = new SensorData(1, v.toString(), 1,1,1) #:: loop(v + 1)
+      loop(0)
+    }
     val schema = Schema.BYTES
 
     val topic = Topic("persistent://sample/standalone/default/in")
@@ -34,9 +36,10 @@ object Main {
     val producer = client.producer[Array[Byte]](producerConfig)(schema)
 
     print("Begin sending messages")
-    iter.foreach(x => {
+    lst.foreach(x => {
       print(s"\nSend message: ${x.sensorId}\n")
       producer.send(x.toByteArray)
+      Thread.sleep(5000)
     })
 
     producer.close()
