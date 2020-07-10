1ab60432cc39ea69707ec2723db16cf7c50a933d
Matthew Alvarez <matt@mattjalvarez.com>
2020-07-09 23:08:40 -0500

    now things should work nicely locally

--- data/src/main/scala/Main.scala
+++ data/src/main/scala/Main.scala
@@ -6,40 +6,31 @@ import scala.util.Random
 import scala.io.Source
 import cdl.iot.SensorData.SensorData
 
-//sensor_id,timestamp,temperature,pressure,wind
-//@SerialVersionUID(100L)
-//class SensorData(sensor_id: Int, _timestamp: String, _temperature: Int, _pressure: Int, _wind: Int)
-// extends Serializable{
-//  val id: Int = sensor_id
-//  val timestamp: String = _timestamp
-//  val temperature: Int = _temperature
-//  val pressure: Int = _pressure
-//  val wind: Int = _wind
-//}
 object Main {
 
+  def env(name: String, default: String): String = if (sys.env.contains(name)) sys.env(name) else default
+
   def main(args: Array[String]) {
+    val sleepIntervalMils = env("INTERVAL_MILS", "5000").toInt
+    val hostName = env("PULSAR_HOST", "localhost")
+    val topicName = env("TOPIC_NAME", "in")
+    val namespace = env("NAMESPACE_NAME", "default")
 
-    val src = Source.fromFile(s"${System.getProperty("user.dir")}/sensor_data.csv")
-    val iter = src.getLines()
-      .drop(1)
-      .map(_.split(","))
-      .map(x => new SensorData(x(0).toInt, x(1), x(2).toDouble, x(3).toDouble, x(4).toDouble))
 
-    val client = PulsarClient("pulsar://localhost:6650")
+    val client = PulsarClient(s"pulsar://${hostName}:6650")
+
 
     val currTime = LocalDateTime.now()
 
     def lst: LazyList[SensorData] = {
       def loop(v: Int): LazyList[SensorData] = {
-        new SensorData(Random.between(5000, 5005), (currTime.plusSeconds(v)).toString(), Random.between(30, 101), Random.between(900, 1201), Random.between(0, 70)) #:: loop(v + 5)
+        new SensorData(Random.between(5000, 5005), (currTime.plusSeconds(v)).toString, Random.between(30, 101), Random.between(900, 1201), Random.between(0, 70)) #:: loop(v + 5)
       }
       loop(0)
     }
 
     val schema = Schema.BYTES
-
-    val topic = Topic("persistent://sample/standalone/default/in")
+    val topic = Topic(s"persistent://sample/standalone/${namespace}/${topicName}")
     val producerConfig = ProducerConfig(topic)
     val producer = client.producer[Array[Byte]](producerConfig)(schema)
 
@@ -47,7 +38,7 @@ object Main {
     lst.foreach(x => {
       print(s"\nSend message: ${x.sensorId} ${x.timestamp} ${x.temperature} ${x.pressure} ${x.wind}\n")
       producer.send(x.toByteArray)
-      Thread.sleep(5000)
+      Thread.sleep(sleepIntervalMils)
     })
 
     producer.close()
--- inference/src/main/scala/org/example/Main.scala
+++ inference/src/main/scala/org/example/Main.scala
@@ -1,24 +1,22 @@
 package org.example
 
 import cdl.iot.SensorData.SensorData
-import javassist.bytecode.ByteArray
 import org.apache.flink.api.common.functions.MapFunction
 import org.apache.flink.api.common.serialization.{DeserializationSchema, SerializationSchema}
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.api.java.tuple
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.pulsar.{FlinkPulsarProducer, PulsarSourceBuilder}
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink
 import org.apache.flink.streaming.connectors.pulsar.partitioner.PulsarKeyExtractor
-import org.example.data.SensorDataModel
 
-//sensor_id,timestamp,temperature,pressure,wind
 
 object Main {
+
   def main(args: Array[String]) {
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment()
+
     val serviceUrl = "pulsar://localhost:6650"
     val inputTopic = "persistent://sample/standalone/default/in"
     val outputTopic = "persistent://public/standalone/default/event-log"
@@ -36,19 +34,17 @@ object Main {
     val inference = input.map(new SensorDataMapper)
 
 
-    //    val p = new FlinkPulsarProducer(
-    //      serviceUrl,
-    //      outputTopic,
-    //      new SensorDataSerializer,
-    //      new SensorDataKeyExtractor
-    //    )
-
     CassandraSink.addSink(inference)
       .setHost(cassandraHost)
       .setQuery("INSERT INTO iot_prototype_training.sensor_data(key, value) values (?, ?);")
       .build()
 
-    //input.addSink(p)
+    input.addSink(new FlinkPulsarProducer(
+      serviceUrl,
+      outputTopic,
+      new SensorDataSerializer,
+      new SensorDataKeyExtractor
+    ))
 
     env.execute("Test Job")
   }
