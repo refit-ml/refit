4aacaca27af367f88ddbc2495b5fe2bfd18a5d75
Nancy Zhang <nancyzyx@gmail.com>
2020-07-17 20:01:10 -0500

    add csv file and modified code for setting up flag

--- models/src/main/scala/baseline/baseline.scala
+++ models/src/main/scala/baseline/baseline.scala
@@ -1,12 +1,15 @@
 package baseline
+import java.io.File
 
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.{Pipeline, PipelineStage}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{to_timestamp, _}
-import org.apache.spark.sql.types.{DoubleType, IntegerType}
-
-import scala.collection.immutable.HashMap
-import scala.util.control.Breaks._
+import org.apache.spark.sql.types.DoubleType
+import org.jpmml.sparkml.PMMLBuilder
 
 
 object baseline_model {
@@ -24,16 +27,11 @@ object baseline_model {
     val file_path = s"${System.getProperty("user.dir")}/operable.csv"
     val time_path = s"${System.getProperty("user.dir")}/time.csv"
 
+
     var data = spark.read.format("CSV").option("header", "true").load(file_path)
     var time = spark.read.format("CSV").option("header", "true").load(time_path)
-    data.show(5)
-    time.show()
-    val count_data = (data.count).toInt
-    val count_time = (time.count).toInt
-
-    // extract hour from timestamp
-    data = data.select(col("*"), substring(col("timestamp"), 9, 2).as("hour"))
-    data = data.withColumn("hour", data("hour").cast(IntegerType))
+//    data.show(5)
+//    time.show()
 
     val ts = to_timestamp(col("timestamp"), "M/dd/yy HH:mm")
     data = data.withColumn("timestamp", ts).sort("timestamp")
@@ -42,6 +40,7 @@ object baseline_model {
     time = time.withColumn("from", time_from).sort("from")
     time = time.withColumn("to", time_to)
 
+    // compare time intervals
     val transformed = data
       .withColumn("end_hour", col("timestamp") + expr("INTERVAL 30 minutes"))
       .join(time,
@@ -51,71 +50,35 @@ object baseline_model {
       )
 
     // This is an example of showing the operable data set
-    transformed.filter(isnull(col("from")) && isnull(col("to"))).show(20)
+    //transformed.filter(isnull(col("from")) && isnull(col("to"))).show(20)
     // this is an example of showing the ineropable data set
-    transformed.filter(not(isnull(col("from")) && isnull(col("to")))).show(20)
-    return
+    //transformed.filter(not(isnull(col("from")) && isnull(col("to")))).show(20)
 
 
-    val dataIndex = data.rdd.zipWithIndex
-    val dataindexKey = dataIndex.map { case (k, v) => (v, k) }
-    val timeIndex = time.rdd.zipWithIndex
-    val timeindexKey = timeIndex.map { case (k, v) => (v, k) }
+    val operableEntries = transformed.filter(isnull(col("from")) && isnull(col("to"))).withColumn("operable",lit(0))
+    val ineropableEntries = transformed.filter(not(isnull(col("from")) && isnull(col("to")))).withColumn("operable",lit(1))
+    var transformedDataSet = operableEntries.union(ineropableEntries)
+    transformedDataSet.sort("timestamp")
 
 
-
-
-
-
-    //    val operableEntries = data.filter(/* Filter to see if they are in the oerable-1 */).map( /* Add operable flag */)
-    //    val ineropableEntries = data.filter(/* filter to see if they are in the operable-o */).map(/* Add operable flag */)
-    //
-    //    val transformedDataSet = operableEntries.union(ineropableEntries)
-
-    for (i <- 1 to 2) {
-      for (j <- 1 to count_time - 1) {
-        breakable {
-          var d_from = (dataindexKey.lookup(i)) (0).getTimestamp(2)
-          var d_to = (dataindexKey.lookup(i)) (0).getTimestamp(7)
-          var t_from = (timeindexKey.lookup(j)) (0).getTimestamp(0)
-          var t_to = (timeindexKey.lookup(j)) (0).getTimestamp(1)
-          var flag = (dataindexKey.lookup(i)) (0).get(8)
-
-          //var flag = data.rdd.take(i).last.getInt(8)
-          //            if (d_from.after(t_from) && d_to.before(t_to)) {
-          //              data.rdd.take(i).last.apply()
-          //              break
-          //            }
-          //            if (d_from.before(t_from) && d_to.before(t_to)) {
-          //              data.rdd.take(i).indexOf(7) = 1
-          //              break
-          //            }
-          //data = data.withColumn("operable", when((col("timestamp") > t_from && col("end_hour")<t_to)
-          //|| (col("timestamp")<t_from && col("end_hour")<t_to),1))
-          if (d_to.before(t_from)) {
-            //var sub = Seq(dataindexKey.lookup(i))(0).toDF()
-          }
-        }
-      }
-    }
-
-    data = data.withColumn("temperature", data("temperature").cast(DoubleType))
-    data = data.withColumn("pressure", data("pressure").cast(DoubleType))
-    data = data.withColumn("wind", data("wind").cast(DoubleType))
-    // sanity check
-    //data = data.withColumn("operable",when(col("temperature")>= 80, 1).otherwise(0))
-
-    data.show(20)
+    transformedDataSet = transformedDataSet.withColumn("temperature", transformedDataSet("temperature").cast(DoubleType))
+    transformedDataSet = transformedDataSet.withColumn("pressure", transformedDataSet("pressure").cast(DoubleType))
+    transformedDataSet = transformedDataSet.withColumn("wind", transformedDataSet("wind").cast(DoubleType))
+    transformedDataSet = transformedDataSet.drop("from")
+    transformedDataSet = transformedDataSet.drop("to")
+    //transformedDataSet.show(20)
 
     // select features
-    /*val assembler = new VectorAssembler()
-      .setInputCols(Array("hour","temperature","pressure","wind"))
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("temperature", "pressure", "wind"))
       .setOutputCol("features")
+      .setHandleInvalid("skip")
 
-    val df1 = assembler.setHandleInvalid("skip").transform(data)
-
+    //    val df1 = assembler.setHandleInvalid("skip").transform(data)
     val labelIndexer = new StringIndexer().setInputCol("operable").setOutputCol("label")
-    val df2 = labelIndexer.fit(df1).transform(df1)*/
+
+
+    //    val df2 = labelIndexer.fit(df1).transform(df1)
 
     //      val output = assembler.transform(data)
     //      println("Assembled columns 'hour','timestamp', 'temperature', 'pressure' ,'wind' to vector column 'features'")
@@ -123,12 +86,12 @@ object baseline_model {
     //      output.select("*")
 
     // split train and test data
-    /*val cnt = df2.count
+    val cnt = transformedDataSet.count
     val testSize = (0.2 * cnt).toInt
     val trainSize = (cnt - testSize).toInt
 
-    val trainDf = df2.sort(monotonically_increasing_id).limit(trainSize)
-    val testDf = df2.sort(monotonically_increasing_id.desc).limit(testSize)
+    val trainDf = transformedDataSet.sort(monotonically_increasing_id).limit(trainSize)
+    val testDf = transformedDataSet.sort(monotonically_increasing_id.desc).limit(testSize)
 
     val classifier = new RandomForestClassifier()
       .setImpurity("gini")
@@ -137,7 +100,10 @@ object baseline_model {
       .setFeatureSubsetStrategy("auto")
       .setSeed(813)
     //println("Training!")
-    val model = classifier.fit(trainDf)
+    val pipeline = new Pipeline().setStages(Array[PipelineStage](labelIndexer, assembler, classifier))
+    val model = pipeline.fit(trainDf)
+    //    val model = classifier.fit(trainDf)
+
     val predictions = model.transform(testDf)
     predictions.select("sensor_id", "label", "prediction").show(5)
 
@@ -150,10 +116,16 @@ object baseline_model {
       .setLabelCol("label")
       .setPredictionCol("prediction")
       .setMetricName("f1")
+
+
     val f1 = evaluator2.evaluate(predictions)
     println("F1 score = " + f1)
-    println("Test Accuracy = " + accuracy)*/
-    //println("Finish!")
-  }
+    println("Test Accuracy = " + accuracy)
 
+
+    val schema = transformedDataSet.schema
+
+
+    val builder = new PMMLBuilder(schema, model).buildFile(new File(s"${System.getProperty("user.dir")}/model.pmml"))
+  }
 }
