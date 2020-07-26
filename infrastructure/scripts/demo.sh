4a90a0e399494c7ff18be6d39a8717ae66848c39
Matthew Alvarez <matt@mattjalvarez.com>
2020-07-26 18:50:09 -0500

    work arounds for model re-driving

--- infrastructure/scripts/demo.sh
+++ infrastructure/scripts/demo.sh
@@ -13,9 +13,9 @@ kubectl port-forward service/cassandra 9000:9042 &
 kubectl port-forward service/flink-jobmanager 8081:8081 &
 kubectl port-forward service/pulsar-standalone 6650:6650 &
 
-
-echo "=== Listen to output ==="
 sleep 10
+echo "=== Listen to output ==="
+
 pulsar-client consume persistent://sample/standalone/ns1/event-log -n 0 -s tst-consumer
 
 
