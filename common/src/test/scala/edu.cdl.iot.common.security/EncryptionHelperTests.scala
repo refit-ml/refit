5475462e06327ce2ef73cba7f864f604402b075f
Matthew Alvarez <matt@mattjalvarez.com>
2020-08-04 22:57:29 -0500

    clean up tests

--- common/src/test/scala/edu.cdl.iot.common.security/EncryptionHelperTests.scala
+++ common/src/test/scala/edu.cdl.iot.common.security/EncryptionHelperTests.scala
@@ -1,24 +1,16 @@
-package edu.cdl.iot.test.common.security
+package edu.cdl.iot.common.security
 
-
-import edu.cdl.iot.common.security.EncryptionHelper
-import javax.crypto.KeyGenerator
-import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-import scala.collection.mutable
 
 class EncryptionHelperTests extends AnyFlatSpec with should.Matchers {
 
   "Encryption" should "Be two way" in {
-    val generator = KeyGenerator.getInstance("AES")
-    generator.init(128)
-    val key = generator.generateKey()
-    val secret = key.getEncoded
+    val key = "keyboard_cat"
     val plaintext = "my_secret"
-    val cyptertext = EncryptionHelper.encrypt(secret, plaintext.getBytes(EncryptionHelper.charset))
-    val decoded = new String(EncryptionHelper.decrypt(secret, cyptertext), EncryptionHelper.charset)
+    val cyptertext = EncryptionHelper.encrypt(key, plaintext)
+    val decoded = EncryptionHelper.decrypt(key, cyptertext)
     decoded should be(plaintext)
   }
 }
