02f2297ee6455a92be722261bf557bcadbaddfce
J Montgomery Maxwell <jmontmaxwell@gmail.com>
2020-08-09 16:20:11 -0500

    Adding KeyedCoProcessFunction

--- inference/src/main/scala/edu/cdl/iot/inference/transform/EvaluationProcessor.scala
+++ inference/src/main/scala/edu/cdl/iot/inference/transform/EvaluationProcessor.scala
@@ -1,26 +1,29 @@
 package edu.cdl.iot.inference.transform
 
 import java.io.ByteArrayInputStream
-import java.util.concurrent.locks.{Lock, ReadWriteLock}
 
 import edu.cdl.iot.inference.util.helpers
 import edu.cdl.iot.protocol.Model.Model
 import edu.cdl.iot.protocol.Prediction.Prediction
 import edu.cdl.iot.protocol.SensorData.SensorData
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.configuration.Configuration
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction
+import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
+import org.apache.flink.streaming.api.functions.co.KeyedCoProcessFunction
 import org.apache.flink.util.Collector
 import org.jpmml.evaluator.{EvaluatorUtil, LoadingModelEvaluatorBuilder, ModelEvaluator}
 
-import scala.collection.JavaConverters.mapAsScalaMapConverter
-import collection.JavaConverters.mapAsJavaMapConverter
+import scala.collection.JavaConverters.{mapAsJavaMapConverter, mapAsScalaMapConverter}
 
-class EvaluationProcessor extends CoProcessFunction[SensorData, Model, Prediction] {
+class EvaluationProcessor extends KeyedCoProcessFunction[String, SensorData, Model, Prediction] with CheckpointedFunction {
 
   private var evaluator: ModelEvaluator[_] = _
   private var modelGuid: String = "__NONE__"
 
-  override def processElement1(value: SensorData, ctx: CoProcessFunction[SensorData, Model, Prediction]#Context, out: Collector[Prediction]): Unit = {
+  var ModelState: ValueState[String] = _
+
+  override def processElement1(value: SensorData, ctx: KeyedCoProcessFunction[String, SensorData, Model, Prediction]#Context, out: Collector[Prediction]): Unit = {
     if (evaluator != null) {
       println("Try make ")
       val p = evaluator.evaluate(helpers.getVector(value).asJava)
@@ -64,18 +67,34 @@ class EvaluationProcessor extends CoProcessFunction[SensorData, Model, Predictio
 
   }
 
-  override def processElement2(model: Model, ctx: CoProcessFunction[SensorData, Model, Prediction]#Context, out: Collector[Prediction]): Unit = {
+  override def processElement2(model: Model, ctx: KeyedCoProcessFunction[String, SensorData, Model, Prediction]#Context, out: Collector[Prediction]): Unit = {
     println(s"Model update: ${model.key}")
     modelGuid = model.key
     evaluator = new LoadingModelEvaluatorBuilder()
       .load(new ByteArrayInputStream(model.bytes.toByteArray))
       .build();
     println("Model updated")
+
+    //this.ModelState.update(model.key)
   }
 
   override def open(conf: Configuration): Unit = {
     evaluator = null
 
+    //val ModelStateDescriptor = new ValueStateDescriptor[String]("ModelState", classOf[String])
+    //ModelState = getRuntimeContext.getState[String](ModelStateDescriptor)
+
+  }
+
+  override def initializeState(context: FunctionInitializationContext): Unit = {
+
+    val ModelStateDescriptor = new ValueStateDescriptor[String]("ModelState", classOf[String])
+    ModelState = getRuntimeContext.getState[String](ModelStateDescriptor)
+  }
+
+  override def snapshotState(snapshotContext: FunctionSnapshotContext): Unit = {
+    ModelState.clear()
+    ModelState.update(modelGuid)
   }
 
 }
