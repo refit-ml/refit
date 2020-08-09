490491575582dec68a879742a7eaa97e3d86dc63
Matthew Alvarez <matt@mattjalvarez.com>
2020-08-08 23:37:53 -0500

    refactor

--- camel/src/main/scala/edu/cdl/iot/camel/CamelMain.scala
+++ camel/src/main/scala/edu/cdl/iot/camel/CamelMain.scala
@@ -1,12 +1,17 @@
 package edu.cdl.iot.camel
 
-import edu.cdl.iot.camel.routes.SensorDataRoutes
+import edu.cdl.iot.camel.routes.{GrafanaRoutes, SensorDataRoutes}
+import org.apache.camel.component.servlet.ServletComponent
 import org.apache.camel.impl.DefaultCamelContext
 
 object CamelMain {
   def main(args: Array[String]) {
     val context = new DefaultCamelContext
+    val sc = new ServletComponent()
+    context.addComponent("servlet", sc)
+
     context.addRoutes(new SensorDataRoutes(context))
+//    context.addRoutes(new GrafanaRoutes(context))
 
     context.start()
   }
--- camel/src/main/scala/edu/cdl/iot/camel/routes/GrafanaRoutes.scala
+++ camel/src/main/scala/edu/cdl/iot/camel/routes/GrafanaRoutes.scala
@@ -2,11 +2,22 @@ package edu.cdl.iot.camel.routes
 
 
 import org.apache.camel.builder.RouteBuilder
-import org.apache.camel.CamelContext
+import org.apache.camel.{CamelContext, LoggingLevel}
+import org.apache.camel.model.rest.RestBindingMode
 
 class GrafanaRoutes(val context: CamelContext) extends RouteBuilder(context) {
   //  restConfiguration()
   override def configure(): Unit = {
+    restConfiguration()
+      .component("servlet")
+      .host("localhost")
+      .bindingMode(RestBindingMode.json)
+      .port(3000)
 
+    rest("/hello")
+      .get()
+      .to("direct:hello")
+
+    from("direct:hello").log(LoggingLevel.INFO, "Hello World").transform.simple("Hello World")
   }
 }
