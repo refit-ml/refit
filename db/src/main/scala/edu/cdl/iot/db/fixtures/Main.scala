4d011262db99e556dfc0ac303acd29a12f6c3133
Nancy Zhang <nancyzyx@gmail.com>
2020-08-09 07:12:25 -0500

    change column name

--- db/src/main/scala/edu/cdl/iot/db/fixtures/Main.scala
+++ db/src/main/scala/edu/cdl/iot/db/fixtures/Main.scala
@@ -13,39 +13,39 @@ object Main {
     val cassandraPassword = "cassandra"
     val cassandraKeyspace = "cdl_refit"
     val loadTrainingWindow = false
-    val loadSensorData = true
+    val loadSensorData = false
 
-    val conf = new SparkConf()
-      .setAppName("baselineModel")
-      .set("spark.cassandra.connection.host", cassandraHost)
-      .set("spark.cassandra.auth.username", cassandraUsername)
-      .set("spark.cassandra.auth.password", cassandraPassword)
-      .setMaster("local[2]")
-    val session = SparkSession.builder.config(conf).getOrCreate()
-    session.sparkContext.setLogLevel("ERROR")
 
     println("Create schema fixtures ...")
     Fixtures.build(cassandraHost, cassandraUsername, cassandraPassword, cassandraKeyspace)
 
+    if (loadTrainingWindow || loadSensorData) {
+      val conf = new SparkConf()
+        .setAppName("baselineModel")
+        .set("spark.cassandra.connection.host", cassandraHost)
+        .set("spark.cassandra.auth.username", cassandraUsername)
+        .set("spark.cassandra.auth.password", cassandraPassword)
+        .setMaster("local[2]")
+      val session = SparkSession.builder.config(conf).getOrCreate()
+      session.sparkContext.setLogLevel("ERROR")
 
-    val schema = Prototype.dummy
+      val schema = Prototype.dummy
 
-    if (loadSensorData) {
-      println("Importing Sensor Data")
-      val data = SensorDataImport.load(session, schema)
-      data.show(5)
-      println("Saving Sensor Data")
-      SensorDataImport.save(data)
+      if (loadSensorData) {
+        println("Importing Sensor Data")
+        val data = SensorDataImport.load(session, schema)
+        data.show(5)
+        println("Saving Sensor Data")
+        SensorDataImport.save(data)
+      }
+
+      if (loadTrainingWindow) {
+        println("Importing training window data")
+        val data = TrainingWindowImport.load(session, schema)
+        data.show(5)
+        println("Saving training window data")
+        TrainingWindowImport.save(data)
+      }
     }
-
-    if (loadTrainingWindow) {
-      println("Importing training window data")
-      val data = TrainingWindowImport.load(session, schema)
-      data.show(5)
-      println("Saving training window data")
-      TrainingWindowImport.save(data)
-    }
-
-
   }
 }
