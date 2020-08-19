78a619627abc186fde02afe4897845443437aa63
Matthew Alvarez <matt@mattjalvarez.com>
2020-08-18 22:20:53 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,8 @@
 set -e
 echo "Begin Job"
 
-JOB_ID=$(flink -m flink-jobmanager:6123 list | sed -n 3p | cut -c23-54)
-SAVEPOINT=$(flink -m flink-jobmanager:6123 savepoint $JOB_ID savepoints/refit/inference | sed -n 3p | cut -c33-)
+JOB_ID=$(flink list | sed -n 3p | cut -c23-54)
+SAVEPOINT=$(flink stop $JOB_ID -p savepoints/refit/inference | sed -n 2p | cut -c33-)
+echo "Start Job \n\t JOB_ID: $JOB_ID\n\t SAVEPOINT: $SAVEPOINT\n\n"
 
-echo "Start Job \n\t JOB_ID: $JOB_ID\n\t SAVEPOINT: $SAVEPOINT"
-
-flink stop -m flink-jobmanager:6123 $JOB_ID
-
-flink run -m flink-jobmanager:6123 -d -s $SAVEPOINT inference/target/scala-2.11/inference.jar
+flink run -s $SAVEPOINT inference/target/scala-2.11/inference.jar -d
