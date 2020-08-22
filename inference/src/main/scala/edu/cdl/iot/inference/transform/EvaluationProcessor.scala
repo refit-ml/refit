01da3dac494d9f14f69e6a8e5c684598d42f43c1
J Montgomery Maxwell <jmontmaxwell@gmail.com>
2020-08-22 16:53:37 -0500

    (De)Serialize evaluator for statefulness

--- inference/src/main/scala/edu/cdl/iot/inference/transform/EvaluationProcessor.scala
+++ inference/src/main/scala/edu/cdl/iot/inference/transform/EvaluationProcessor.scala
@@ -6,6 +6,7 @@ import edu.cdl.iot.inference.util.helpers
 import edu.cdl.iot.protocol.Model.Model
 import edu.cdl.iot.protocol.Prediction.Prediction
 import edu.cdl.iot.protocol.SensorData.SensorData
+import org.apache.commons.lang3.SerializationUtils
 import org.apache.flink.api.common.state.{MapState, MapStateDescriptor}
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
@@ -14,14 +15,13 @@ import org.apache.flink.streaming.api.functions.co.KeyedCoProcessFunction
 import org.apache.flink.util.Collector
 import org.jpmml.evaluator.{EvaluatorUtil, LoadingModelEvaluatorBuilder, ModelEvaluator}
 
-import scala.collection.JavaConverters.{mapAsJavaMapConverter, mapAsScalaMapConverter}
-import scala.collection.JavaConverters._
+import scala.collection.JavaConverters.{mapAsJavaMapConverter, mapAsScalaMapConverter, _}
 
 class EvaluationProcessor extends KeyedCoProcessFunction[String, SensorData, Model, Prediction] with CheckpointedFunction {
 
   private var evaluators: Map[String, ModelEvaluator[_]] = _
   private var models: Map[String, String] = _
-  private var evaluatorState: MapState[String, ModelEvaluator[_]] = _
+  private var evaluatorState: MapState[String, Array[Byte]] = _
   private var modelState: MapState[String, String] = _
 
   override def processElement1(value: SensorData, ctx: KeyedCoProcessFunction[String, SensorData, Model, Prediction]#Context, out: Collector[Prediction]): Unit = {
@@ -95,33 +95,44 @@ class EvaluationProcessor extends KeyedCoProcessFunction[String, SensorData, Mod
     val modelStateDescriptor = new MapStateDescriptor[String, String]("ModelState", classOf[String], classOf[String])
     modelState = getRuntimeContext.getMapState[String, String](modelStateDescriptor)
 
-    val evaluatorStateDescriptor = new MapStateDescriptor[String, ModelEvaluator[_]]("EvaluatorState", classOf[String], classOf[ModelEvaluator[_]])
-    evaluatorState = context.getKeyedStateStore.getMapState[String, ModelEvaluator[_]](evaluatorStateDescriptor)
-
+    val evaluatorStateDescriptor = new MapStateDescriptor[String, Array[Byte]]("EvaluatorState", classOf[String], classOf[Array[Byte]])
+    evaluatorState = context.getKeyedStateStore.getMapState[String, Array[Byte]](evaluatorStateDescriptor)
 
     if(context.isRestored){
-      val modelKeys = modelState.keys().asScala
-      for(key <- modelKeys){
+
+      val modelStateKeys = modelState.keys().asScala
+      for(key <- modelStateKeys){
         models += (key -> modelState.get(key))
       }
 
-      val evaluatorKeys = evaluatorState.keys().asScala
-      for(key <- evaluatorKeys){
-        evaluators += (key -> evaluatorState.get(key))
+      val evaluatorStateKeys = evaluatorState.keys().asScala
+      for(key <- evaluatorStateKeys){
+        val evaluatorBytes = evaluatorState.get(key)
+        val evaluator: ModelEvaluator[_] = SerializationUtils.deserialize(evaluatorBytes)
+        evaluators += (key -> evaluator)
       }
-    }
 
+    }
 
   }
 
   override def snapshotState(ctx: FunctionSnapshotContext): Unit = {
+
     val id = ctx.getCheckpointId
     println(s"Checkpointing: ${id}")
+
     modelState.clear()
     evaluatorState.clear()
 
     models.foreach(model => modelState.put(model._1, model._2))
-    evaluators.foreach(evaluator => evaluatorState.put(evaluator._1, evaluator._2))
+
+    val evaluatorKeys = evaluators.keys
+    for(key <- evaluatorKeys){
+      val evaluator = evaluators(key)
+      val evaluatorBytes: Array[Byte] = SerializationUtils.serialize(evaluator.isInstanceOf[Serializable])
+      evaluatorState.put(key, evaluatorBytes)
+    }
+
   }
 
 }
