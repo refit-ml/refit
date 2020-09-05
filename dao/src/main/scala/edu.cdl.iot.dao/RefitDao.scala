af79d74039f618886d3853dec07ab865f4987a0e
Matthew Alvarez <matt@mattjalvarez.com>
2020-09-05 09:51:25 -0500

    adding refit dao

---
+++ dao/src/main/scala/edu.cdl.iot.dao/RefitDao.scala
@@ -0,0 +1,30 @@
+package edu.cdl.iot.dao
+
+import com.datastax.driver.core.{Cluster, HostDistance, PoolingOptions, Session}
+import edu.cdl.iot.common.yaml.CassandraConfig
+
+class RefitDao(private val config: CassandraConfig) {
+  lazy val poolingOptions: PoolingOptions = {
+    new PoolingOptions()
+      .setConnectionsPerHost(HostDistance.LOCAL, 4, 10)
+      .setConnectionsPerHost(HostDistance.REMOTE, 2, 4)
+  }
+
+  lazy val cluster: Cluster = {
+    val builder = Cluster.builder()
+    builder.addContactPoint(config.host)
+    builder.withCredentials(config.user, config.password)
+    builder.withPort(config.port)
+    builder.build()
+  }
+
+  var session: Session = cluster.connect()
+
+
+  def close(): Unit = {
+    session.close()
+    cluster.close()
+    session = null
+  }
+
+}
