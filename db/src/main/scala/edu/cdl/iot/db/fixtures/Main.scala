7ebdb03387f9e9cdb1aa8ea9cc1e0594e0ac6ad2
Matthew Alvarez <matt@mattjalvarez.com>
2020-09-06 19:23:03 -0500

    add my work

--- db/src/main/scala/edu/cdl/iot/db/fixtures/Main.scala
+++ db/src/main/scala/edu/cdl/iot/db/fixtures/Main.scala
@@ -9,43 +9,52 @@ import org.joda.time.DateTime
 
 
 object Main {
-  val loadTrainingWindow = false
-  val loadSensorData = false
+  val loadTrainingWindow = true
+  val loadSensorData = true
   val schemaDirectory = "SCHEMA_DIRECTORY"
 
   def main(args: Array[String]): Unit = {
-    val configFactory = new ConfigFactory()
-    val config = configFactory.getConfig
 
-    val fixtureDao = new FixtureDao(config.getCassandraConfig())
+    try {
+      val configFactory = new ConfigFactory()
+      val config = configFactory.getConfig
+
+      val fixtureDao = new FixtureDao(config.getCassandraConfig())
 
 
-    val schemas =
-      if (sys.env.contains(schemaDirectory))
-        SchemaFactory.getSchemas(sys.env(schemaDirectory))
-      else SchemaFactory.getSchemas
+      val schemas =
+        if (sys.env.contains(schemaDirectory))
+          SchemaFactory.getSchemas(sys.env(schemaDirectory))
+        else SchemaFactory.getSchemas
 
-    
-    val orgs = schemas.map(x => Org(x.orgGuid,
-      TimestampHelper.toTimestamp(DateTime.now()),
-      x.org
-    ))
-    val projects = schemas.map(Project.fromSchema)
 
-    println("Create Orgs")
-    orgs.foreach(fixtureDao.createOrg)
-    println("Create Projects")
-    projects.foreach(fixtureDao.createProject)
+      val orgs = schemas.map(x => Org(x.orgGuid,
+        TimestampHelper.toTimestamp(DateTime.now()),
+        x.org
+      ))
+      val projects = schemas.map(Project.fromSchema)
 
-    if (loadSensorData || loadTrainingWindow) {
-      println("Import data")
-      schemas.foreach(schema => {
-        ImportHelper.importData(config, fixtureDao, schema, loadSensorData, loadTrainingWindow)
-      })
+      println("Create Orgs")
+      orgs.foreach(fixtureDao.createOrg)
+      println("Create Projects")
+      projects.foreach(fixtureDao.createProject)
+
+      if (loadSensorData || loadTrainingWindow) {
+        println("Import data")
+        schemas.foreach(schema => {
+          ImportHelper.importData(config, fixtureDao, schema, loadSensorData, loadTrainingWindow)
+        })
+      }
+
+      println("Done")
+      fixtureDao.close()
     }
-
-    println("Done")
-    fixtureDao.close()
+    catch {
+      case e: Exception =>
+        println("There was an error importing data")
+        System.exit(1)
+    }
+    System.exit(0)
   }
 
 }
