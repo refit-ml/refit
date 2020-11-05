432eba6fc623f3d1ef475462ad1321d9a84e7156
Matthew Alvarez <matt@mattjalvarez.com>
2020-11-05 00:27:08 -0600

    python enrichment works!

---
+++ inference/src/main/python/feature_extractors/functions.py
@@ -0,0 +1,99 @@
+from pandas import Series, DataFrame
+from pyflink.table import DataTypes
+from pyflink.table.udf import udf
+import pandas as pd
+
+
+@udf(input_types=[DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING()],
+     result_type=DataTypes.STRING(), udf_type='pandas')
+def doubles(project_guid: Series,
+            sensor_id: Series,
+            timestamp: Series,
+            doubles: Series,
+            strings: Series,
+            integers: Series,
+            labels: Series) -> DataFrame:
+    df = pd.DataFrame({'project_guid': project_guid,
+                       'sensor_id': sensor_id,
+                       'timestamp': timestamp,
+                       'doubles': doubles,
+                       'strings': strings,
+                       'integers': integers,
+                       'labels': labels})
+
+    return df['doubles']
+
+
+@udf(input_types=[DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING()],
+     result_type=DataTypes.STRING(), udf_type='pandas')
+def strings(project_guid: Series,
+            sensor_id: Series,
+            timestamp: Series,
+            doubles: Series,
+            strings: Series,
+            integers: Series,
+            labels: Series) -> DataFrame:
+    df = pd.DataFrame({'project_guid': project_guid,
+                       'sensor_id': sensor_id,
+                       'timestamp': timestamp,
+                       'doubles': doubles,
+                       'strings': strings,
+                       'integers': integers,
+                       'labels': labels})
+
+    return df['strings']
+
+
+@udf(input_types=[DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING()],
+     result_type=DataTypes.STRING(), udf_type='pandas')
+def integers(project_guid: Series,
+             sensor_id: Series,
+             timestamp: Series,
+             doubles: Series,
+             strings: Series,
+             integers: Series,
+             labels: Series) -> DataFrame:
+    df = pd.DataFrame({'project_guid': project_guid,
+                       'sensor_id': sensor_id,
+                       'timestamp': timestamp,
+                       'doubles': doubles,
+                       'strings': strings,
+                       'integers': integers,
+                       'labels': labels})
+    print("WE ARE HERE")
+    print("DF")
+    print(df)
+    df['integers'] = df['integers'].apply(lambda i: "{\"test-int\": 1}")
+    return df['integers']
+
+
+@udf(input_types=[DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING(), DataTypes.STRING(),
+                  DataTypes.STRING()],
+     result_type=DataTypes.STRING(), udf_type='pandas')
+def labels(project_guid: Series,
+           sensor_id: Series,
+           timestamp: Series,
+           doubles: Series,
+           strings: Series,
+           integers: Series,
+           labels: Series) -> DataFrame:
+    df = pd.DataFrame({'project_guid': project_guid,
+                       'sensor_id': sensor_id,
+                       'timestamp': timestamp,
+                       'doubles': doubles,
+                       'strings': strings,
+                       'integers': integers,
+                       'labels': labels})
+
+    return df['labels']
--- inference/src/main/python/main.py
+++ inference/src/main/python/main.py
@@ -1,63 +1,62 @@
-from pandas import DataFrame, Series
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import StreamTableEnvironment, DataTypes, EnvironmentSettings
-from pyflink.table.udf import udf
-
-from feature_extractors.demo_feature_extractor import DemoFeatureExtractor
-import pandas as pd
-
-
-@udf(input_types=[DataTypes.STRING(), DataTypes.STRING(),
-                  DataTypes.STRING(), DataTypes.STRING(),
-                  DataTypes.STRING(), DataTypes.STRING(),
-                  DataTypes.STRING()],
-     result_type=DataTypes.STRING(), udf_type='pandas')
-def doubles(project_guid: Series,
-            sensor_id: Series,
-            timestamp: Series,
-            doubles: Series,
-            strings: Series,
-            integers: Series,
-            labels: Series) -> DataFrame:
-    df = pd.DataFrame({'project_guid': project_guid,
-                       'sensor_id': sensor_id,
-                       'timestamp': timestamp,
-                       'doubles': doubles,
-                       'strings': strings,
-                       'integers': integers,
-                       'labels': labels})
-
-    return df['doubles']
+from pyflink.table import StreamTableEnvironment, EnvironmentSettings
 
 
 class RefitFeatureEnrichment():
     def __init__(self):
-        self.feature_extractor = DemoFeatureExtractor()
+        # self.feature_extractor = DemoFeatureExtractor()
         self.settings = EnvironmentSettings.new_instance().in_streaming_mode().use_blink_planner().build()
         self.env = StreamExecutionEnvironment.get_execution_environment()
         self.env.set_parallelism(1)
         self.table_env = StreamTableEnvironment.create(self.env, environment_settings=self.settings)
         self.table_env.get_config().get_configuration().set_boolean("python.fn-execution.memory.managed", True)
+        self.table_env.add_python_file('feature_extractors')
 
-        source_table = open('source.sql', 'r').read()
-        sink_table = open('sink.sql', 'r').read()
+        source_table = open('feature_extractors/source.sql', 'r').read()
+        sink_table = open('feature_extractors/sink.sql', 'r').read()
 
         self.table_env.execute_sql(source_table)
         self.table_env.execute_sql(sink_table)
-        self.table_env.register_function("doubles", doubles)
 
-    def run(self):
+    def run_udf(self):
+        from feature_extractors.functions import doubles, strings, integers, labels
+        self.table_env.register_function("doubles", doubles)
+        self.table_env.register_function("strings", strings)
+        self.table_env.register_function("integers", integers)
+        self.table_env.register_function("labels", labels)
+
         self.table_env.from_path('refit_raw_sensor_data') \
             .select("projectGuid, "
                     "sensorId, "
                     "timestamp, "
-                    "doubles(projectGuid, sensorId, timestamp, doubles, strings, integers, labels),"
-                    "strings, "
-                    "integers, "
-                    "labels") \
+                    "doubles(projectGuid, sensorId, timestamp, doubles, strings, integers, labels) as doubles,"
+                    "strings(projectGuid, sensorId, timestamp, doubles, strings, integers, labels) as strings,"
+                    "integers(projectGuid, sensorId, timestamp, doubles, strings, integers, labels) as integers,"
+                    "labels(projectGuid, sensorId, timestamp, doubles, strings, integers, labels) as labels") \
             .execute_insert('refit_sensor_data')
 
         self.env.execute("CDL IoT - Feature Extraction")
 
+    # WIP, currently not working
+    def run(self):
+        from feature_extractors import functions
+        self.table_env.register_function("doubles", functions.doubles)
 
-RefitFeatureEnrichment().run()
+        df = self.table_env.from_path('refit_raw_sensor_data') \
+            .select("projectGuid, "
+                    "sensorId, "
+                    "timestamp, "
+                    "doubles,"
+                    "strings, "
+                    "integers, "
+                    "labels") \
+            .to_pandas()
+
+        self.table_env \
+            .from_pandas(df, ["projectGuid", "sensorId", "timestamp", "doubles", "strings", "integers", "labels"]) \
+            .execute_insert("refit_sensor_data").get_job_client().get_job_execution_result().result()
+
+        self.env.execute("CDL IoT - Feature Extraction")
+
+
+RefitFeatureEnrichment().run_udf()
