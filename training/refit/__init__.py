52cbfd68d537b5dd71f2c87559cec4c76cf83cc8
J Montgomery Maxwell <jmontmaxwell@gmail.com>
2021-02-25 10:59:37 -0600

    df imports`

--- training/refit/__init__.py
+++ training/refit/__init__.py
@@ -97,7 +97,41 @@ class Refit:
     def __get_file_path(self, object_name: str):
         return f"import/{self.project_guid}/{object_name}"
 
-    def import_file(self,
+    def import_df(self,
+                  df_path: str,
+                  object_name: str,
+                  df_format: str) -> str:
+
+        if df_format == 'CSV':
+            return self.__import_file(df_path, object_name)
+
+        elif df_format == 'API':
+            response = urllib.request.urlopen(df_path)
+            data = response.read()
+            df = pd.read_csv(io.BytesIO(data), encoding='utf8')
+            #            if 'TimeStamp' not in df.columns:
+            #                raise Exception("Error file lacks timestamps")
+            #            if 'UniqueID' not in df.columns:
+            #                raise Exception("Error file lacks unique ID's")
+            temp_csv_path = 'temp_csv_path.csv'
+            df.to_csv(temp_csv_path, index = False, header = True )
+            return self.__import_file(temp_csv_path, object_name)
+
+        else:
+            raise Exception("Data frame format", df_format, "is not supported")
+
+    def import_static_data(self,
+                           file_path: str,
+                           object_name: str,
+                           delete_when_complete: bool = True) -> str:
+        path = self.__get_file_path(object_name)
+        if not self.file_repository.upload_file(self._import_bucket, path, file_path):
+            raise Exception("Error Uploading file to bucket")
+
+        return self.notebook_repository.import_file(self.project_guid, path, delete_when_complete)
+
+
+    def __import_file(self,
                     file_path: str,
                     object_name: str,
                     delete_when_complete: bool = True, ) -> str:
