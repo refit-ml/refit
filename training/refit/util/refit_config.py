4c82d393f1f40f405573ab16069756fae1836c00
Matthew Alvarez <matt@mattjalvarez.com>
2021-03-10 00:09:46 -0600

    fix minio host

--- training/refit/util/refit_config.py
+++ training/refit/util/refit_config.py
@@ -3,7 +3,7 @@ import os
 
 class RefitConfig():
     def __init__(self):
-        self.minio_host = os.environ['MINIO_HOST']
+        self.minio_host = os.environ['MINIO_HOST'].replace('http://', '')
         self.minio_access_key = os.environ['MINIO_ACCESS_KEY']
         self.minio_secret_key = os.environ['MINIO_SECRET_KEY']
         self.minio_bucket_import = os.environ['MINIO_BUCKET_IMPORT']
